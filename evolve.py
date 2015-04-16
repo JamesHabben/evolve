499a7621c5a23d6ca94a02cb0dfc2615d5749c7f
James Habben <james@wmif.net>
2015-04-15 19:02:04 -0700

    load

---
+++ evolve.py
@@ -0,0 +1,164 @@
+__author__ = 'james.habben'
+#evpath = 'file:///Users/jameshabben/Documents/Evidence Files/GST/PhysicalMemory'
+#dbpath = evpath + '.sqlite'
+
+evolveVersion = '1.0'
+
+import argparse
+argParser = argparse.ArgumentParser(description='Web interface for Volatility Framework.')
+argParser.add_argument('-f', '--file', help='RAM dump to analyze')
+argParser.add_argument('-l', '--local', help='Restrict webserver to serving \'localhost\' only')
+argParser.add_argument('-p', '--profile', help='Memory profile to use with Volatility')
+args = argParser.parse_args()
+
+if not args.file:
+    #raise BaseException("RAM dump file is required.")
+    print "RAM dump file is required."
+    exit()
+
+import sys
+import volatility
+import bottle
+import sqlite3
+import json
+
+import volatility.constants as constants
+import volatility.registry as registry
+import volatility.commands as commands
+import volatility.conf as conf
+import volatility.obj as obj
+import volatility.plugins.taskmods as taskmods
+import volatility.addrspace as addrspace
+import volatility.utils as utils
+import volatility.renderers as render
+import volatility.plugins as plugins
+#import volatility.plugins.filescan as filescan
+#from volatility.plugins import *
+from bottle import route, Bottle, run, request
+
+Plugins = {'plugins':[]}
+def BuildPluginList():
+    Plugins['plugins'] = []
+    #cmds = registry.get_plugin_classes(commands.Command, lower = True)
+    con = sqlite3.connect(config.OUTPUT_FILE)
+    curs = con.cursor()
+    for cmdname in sorted(cmds):
+        command = cmds[cmdname]
+        if command.is_valid_profile(profile):
+            curs.execute("select name from sqlite_master where type = 'table' and name like ?;", (cmdname,))
+            val=0
+            if curs.fetchone() is not None:
+                val=1
+            Plugins['plugins'].append({'name':cmdname,'data':val,'help':command.help()})
+    curs.close()
+    con.close()
+
+config = conf.ConfObject()
+registry.PluginImporter()
+#config = conf.ConfObject()
+registry.register_global_options(config, addrspace.BaseAddressSpace)
+registry.register_global_options(config, commands.Command)
+config.parse_options(False)
+
+profs = registry.get_plugin_classes(obj.Profile)
+if args.profile:
+    config.PROFILE = args.profile
+else:
+    config.PROFILE = "WinXPSP2x86"
+if config.PROFILE not in profs:
+    #raise BaseException("Invalid profile " + config.PROFILE + " selected")
+    print "Invalid profile " + config.PROFILE + " selected"
+    exit()
+args.file = "file://" + args.file
+config.LOCATION = args.file #evpath
+config.OUTPUT_FILE = args.file +".sqlite"
+config.parse_options()
+profile = profs[config.PROFILE]()
+
+
+
+#print args.file
+
+cmds = registry.get_plugin_classes(commands.Command, lower = True)
+BuildPluginList()
+
+print "Volatility Version: " + constants.VERSION
+
+#test = ""
+
+@route('/')
+def index():
+    return bottle.static_file('eVOLve.htm',root='web')
+
+@route('/web/:path#.+#', name='web')
+def static(path):
+    return bottle.static_file(path, root='web')
+
+@route('/data/plugins')
+def ajax_plugins():
+    BuildPluginList()
+    return json.dumps(Plugins)
+
+@route('/data/volversion')
+def vol_version():
+    return constants.VERSION
+
+@route('/data/evolveversion')
+def evolve_version():
+    return evolveVersion
+
+@route('/data/filepath')
+def evolve_version():
+    return config.LOCATION
+
+@route('/data/profilename')
+def evolve_version():
+    return config.PROFILE
+
+@route('/data/view/<name>', method='GET')
+@route('/data/view/<name>', method='POST')
+def plugin_data(name):
+    result = {"columns":[],"data":[]}
+    con = sqlite3.connect(config.OUTPUT_FILE)
+    if request.method == 'POST' and request.forms.get('query'):
+        query = request.forms.get('query')
+    else:
+        query = "SELECT * FROM " + name
+    curs = con.cursor()
+    try:
+        curs.execute(query)
+        result["data"] = curs.fetchall()
+        result["columns"] = [i[0] for i in curs.description]
+    except Exception as err:
+        result["error"] = err.message
+    result["name"] = name
+    result["query"] = query
+    return json.dumps(result)
+
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+
+@route('/data2/plugin/<name>')
+def plugin_data2(name):
+    con = sqlite3.connect(config.OUTPUT_FILE)
+    curs = con.cursor()
+    con.row_factory = dict_factory
+    cur = con.cursor()
+    cur.execute("SELECT * FROM " + name)
+    return json.dumps( cur.fetchall())
+
+@route('/run/plugin/<name>')
+def run_plugin(name):
+    config.parse_options()
+    command = cmds[name](config)
+    calc = command.calculate()
+    command.render_sqlite(config.OUTPUT_FILE, calc)
+    return "done"
+
+app = Bottle()
+run(host='0.0.0.0', port=8080)
+
+
---
+++ web/evolve.js
@@ -0,0 +1,182 @@
+function GetPluginList () {
+    $.ajax({url:'data/plugins',dataType:'json'}).success(
+        function (data) {
+            $("#pluglist").html("")
+            $.each($.grep(data.plugins,
+                function(d,i) {
+                    return d.data == 1
+                }),
+                function(k,v) {
+                    $("#pluglist").append(
+                    '<div class="plugindata" title="' + v.help + '">' +
+                    v.name +
+                    '<span class="pluginshowdata" onclick="GetData(\'' + v.name + '\')">' +
+                    'show' +
+                    '</span></div>')
+                }
+            )
+            $.each($.grep(data.plugins,
+                function(d,i) {
+                    return d.data == 0
+                }),
+                function(k,v) {
+                    $("#pluglist").append(
+                    '<div class="plugin" title="' + v.help + '">' +
+                    v.name +
+                    '<span class="pluginrun" onclick="RunPlugin(\'' + v.name + '\',this)">' +
+                    'run' +
+                    '</span></div>')
+                }
+            )
+        }
+    )
+}
+
+$.ajax({url:'data/volversion'}).success(
+    function (data) {
+        $("#volver").html(data)
+    }
+)
+
+$.ajax({url:'data/evolveversion'}).success(
+    function (data) {
+        $("#evover").html('v' + data)
+    }
+)
+
+$.ajax({url:'data/filepath'}).success(
+    function (data) {
+        $("#dumpfile").html( data)
+    }
+)
+
+$.ajax({url:'data/profilename'}).success(
+    function (data) {
+        $("#profile").html( data)
+    }
+)
+
+function GetData (pluginname) {
+    $.getJSON('/data/view/' + pluginname , function(data) {
+        ShowData(data)
+    })
+}
+
+function ShowData (data) {
+    //$.getJSON('/data/view/' + pluginname , function(data) {
+        var tbl_body = '';
+        var odd_even = false;
+        tbl_body = '<tr>'
+        $.each(data.columns, function(k, v) {
+            tbl_body += '<td class="datatablehdr">' + v + '</td>'
+        })
+        tbl_body += '</tr>'
+        $.each(data.data, function() {
+            var tbl_row = ""
+            $.each(this, function(k, v) {
+                tbl_row += '<td>' + v + '</td>'
+            })
+            tbl_body += '<tr class="' + (odd_even ? 'odd' : 'even') + '">' + tbl_row + '</tr>'
+            odd_even = !odd_even
+        })
+        $("#datahdr").html(data.name)
+        $("#datasql").val(data.query)
+        $("#datatable").html(tbl_body)
+        $("#dataview").css({"display":"inline"})
+    //})
+}
+
+function ShowDataFancy (pluginname) {
+    $.getJSON('/data2/plugin/' + pluginname , function(data) {
+        colarray = []
+        $.each(data.columns, function(k, v) {
+            colarray.push(v)
+        })
+        $("#datatable").jqGrid({
+            colModel: data.columns,
+            colNames: colarray,
+            datatype: 'clientside',
+            data: data.data
+        })
+    })
+}
+
+function RunPlugin (pluginname, callingobj) {
+    $(callingobj).html('running')
+    $(callingobj).css({'background':'lightgrey'})
+    $.ajax({url:'/run/plugin/' + pluginname})
+    setTimeout(GetPluginList, 500)
+}
+
+var SqlShown = false
+
+function ShowSql () {
+    if (!SqlShown) {
+        $("#sqlcontainer").css({"display":"block"})
+        $("#showsqlbutton").html("Hide SQL")
+    }
+    else {
+        $("#sqlcontainer").css({"display":"none"})
+        $("#showsqlbutton").html("Show SQL")
+    }
+    SqlShown = !SqlShown
+}
+
+function RunSql () {
+    $("#sqlerror").text()
+    $("#sqlerror").css({'display':'none'})
+    senddata = {"query": $("#datasql").val()}
+    $.ajax({url:'/data/view/' + $("#datahdr").text(),
+            method: 'post',
+            data: senddata,
+            dataType:'json'
+            }).success(function (data) {
+                if (data.error) {
+                    $("#sqlerror").text(data.error)
+                    $("#sqlerror").css({'display':'block'})
+                }
+                else {
+                    ShowData(data)
+                }
+            })
+}
+
+$(function() {
+    //  changes mouse cursor when highlighting loawer right of box
+    $("textarea").mousemove(function(e) {
+        var myPos = $(this).offset();
+        myPos.bottom = $(this).offset().top + $(this).outerHeight();
+        myPos.right = $(this).offset().left + $(this).outerWidth();
+
+        if (myPos.bottom > e.pageY && e.pageY > myPos.bottom - 16 && myPos.right > e.pageX && e.pageX > myPos.right - 16) {
+            $(this).css({ cursor: "nw-resize" });
+        }
+        else {
+            $(this).css({ cursor: "" });
+        }
+    })
+    //  the following simple make the textbox "Auto-Expand" as it is typed in
+    .keyup(function(e) {
+        //  this if statement checks to see if backspace or delete was pressed, if so, it resets the height of the box so it can be resized properly
+        if (e.which == 8 || e.which == 46) {
+            $(this).height(parseFloat($(this).css("min-height")) != 0 ? parseFloat($(this).css("min-height")) : parseFloat($(this).css("font-size")));
+        }
+        //  the following will help the text expand as typing takes place
+        while($(this).outerHeight() < this.scrollHeight + parseFloat($(this).css("borderTopWidth")) + parseFloat($(this).css("borderBottomWidth"))) {
+            $(this).height($(this).height()+1);
+        };
+    });
+});
+
+$(document).ready(function() {
+    GetPluginList()
+    $("#showsqlbutton").click(ShowSql)
+    $("#runsqlbutton").click(RunSql)
+    $("#datasql").on("keydown", this, function (event) {
+        if (event.keyCode == 116) {
+            RunSql()
+            return false;
+        }
+    })
+    $("#")
+})
